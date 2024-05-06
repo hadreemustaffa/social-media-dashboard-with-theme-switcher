aea8fb242f2eaf6c76cec0b72d7a692aff58a927
hadreemustaffa <hadree.mustaffa@gmail.com>
2024-05-07 03:03:12 +0800

    feat: enable theme switch using keyboard

--- index.js
+++ index.js
@@ -2,12 +2,20 @@ const slider = document.querySelector('.slider');
 const inputBtn = document.querySelector('#toggleBtn');
 const html = document.documentElement;
 
-slider.addEventListener('click', () => {
-  inputBtn.toggleAttribute('checked');
-
+const setThemeState = () => {
   if (html.getAttribute('color-mode') == 'light') {
+    inputBtn.checked = true;
     html.setAttribute('color-mode', 'dark');
   } else {
+    inputBtn.checked = false;
     html.setAttribute('color-mode', 'light');
   }
+};
+
+slider.addEventListener('click', () => {
+  setThemeState();
+});
+
+inputBtn.addEventListener('change', () => {
+  setThemeState();
 });
